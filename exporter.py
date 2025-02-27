81a71f317079524fdf9283c2d583f1d827547025
mooinglemur <mooinglemur@users.noreply.github.com>
2025-02-26 22:35:56 -0700

    handle null passenger temp setting

--- exporter.py
+++ exporter.py
@@ -194,7 +194,7 @@ class TeslaFiCollector(object):
             labels=label_keys)
         teslafi_passenger_set_temperature.add_metric(
             labels=label_values, 
-            value=float(self.getSetData(teslafi_data, teslafi_data_old, "passenger_temp_setting")))
+            value=float(self.getSetData(teslafi_data, teslafi_data_old, "passenger_temp_setting", -1000)))
         metrics.append(teslafi_passenger_set_temperature)
 
         teslafi_fan_status = GaugeMetricFamily(
